f21e24458cca5e461e126d62e86e0678bfe1e217
Durva <durvac1212@gmail.com>
2025-06-05 10:34:24 -0500

    Added Second Navigation file

--- Navigation/ContentView.swift
+++ Navigation/ContentView.swift
@@ -14,18 +14,29 @@ struct ContentView: View {
                 Text("This is the root view 🏝️ ")
                     .fontWeight(.medium)
                     .foregroundColor(Color.black)
-                NavigationLink(destination: Text("You've arrived at the second View")) {
+                NavigationLink(destination: Second_View()) {
                     Text("Click me")
                 }
-            }//End Vstack
+                        
+                       
+                        
+                        NavigationLink(destination: Text("You've arrived at the third View")) {
+                            Text("Click me 2.0")
+                        }
+                        
+                    
+                }//End Vstack
+            .navigationTitle("Home")
+            .navigationBarTitleDisplayMode(.inline)
+            .navigationBarHidden(true)
             
-        }//End NavigationStack
+            }//End NavigationStack
+            
+            
+        }//End body
         
-        
-    }//End body
+    }//End Struc
     
-}//End Struc
-
-#Preview {
-    ContentView()
-}
+    #Preview {
+        ContentView()
+    }
---
+++ Navigation/Second View.swift
@@ -0,0 +1,18 @@
+//
+//  Second View.swift
+//  Navigation
+//
+//  Created by Scholar on 6/5/25.
+//
+
+import SwiftUI
+
+struct Second_View: View {
+    var body: some View {
+        Text("You've arrived in the second view")
+    }
+}
+
+#Preview {
+    Second_View()
+}
